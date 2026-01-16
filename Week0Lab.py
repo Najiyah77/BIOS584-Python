f185bcd8a055efc3b95985d19bcdf52b41ab5086
Najiyah Williamson <najiyah.williamson@emory.edu>
2026-01-16 12:27:08 -0500

    Week0Lab

---
+++ Week0Lab.py
@@ -0,0 +1,19 @@
+#-----------------------------------------
+#Week 0
+#Name: Najiyah Williamson
+#Date: 1/16/26
+#-----------------------------------------
+
+name = "Najiyah Williamson"
+print(name)
+
+
+data = 5
+print("My favorite number is", data)
+
+#-----------------------------------------
+# F - strings
+#-----------------------------------------
+
+print(f"My favorite number is {data}")
+print(f"Did you know that {data} cubed is {data * data * data}?")
