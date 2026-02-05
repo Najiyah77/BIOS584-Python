b2eeec855c7b3a593e63d03dd3ef90f2114acaaf
Najiyah Williamson <najiyah.williamson@emory.edu>
2026-02-05 18:48:04 -0500

    adding new assignments

--- Week 2 Textbook Exercises_Williamson.py
+++ Week 2 Textbook Exercises_Williamson.py
@@ -121,19 +121,51 @@ countries.sort(reverse=True) #reverse-alphabetical order
 print(countries) #changed the list so that it is in reverse-alphabetical order
 
 #------------------------ Exercise 3-11 ----------------------------
-# 3-11. Intentional Error: If you haven’t received an index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+# 3-11. Intentional Error: If you haven’t received an index error in one of your programs yet, try to make one happen.
+# Change an index in one of your programs to produce an index error. Make sure you correct the error before closing
+# the program.
 
-
+my_list  = [1,2,3]
+#print(my_list[5]) #provides an index error about being out of range
 
 #------------------------ Exercise 4-1 ----------------------------
-# 4-1. Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a list, and then use a for loop to print the name of each pizza.
-# Modify your for loop to print a sentence using the name of the pizza, instead of printing just the name of the pizza. For each pizza, you should have one line of output containing a simple statement like I like pepperoni pizza.
-# Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
+# 4-1. Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a list, and then use a
+# for loop to print the name of each pizza.
+# Modify your for loop to print a sentence using the name of the pizza, instead of printing just the name of the pizza.
+# For each pizza, you should have one line of output containing a simple statement like I like pepperoni pizza.
+# Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should
+# consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
 
+pizzas = ['White', 'Olive', 'Plain']
 
+#for pizza in pizzas:
+    #print(pizza)
+
+for pizza in pizzas:
+    print(f"I like {pizza} pizzas!")
+
+print("I really love pizza!")
 
 #------------------------ Exercise 4-11 ----------------------------
-# 4-11. My Pizzas, Your Pizzas: Start with your program from Exercise 4-1 (page 56). Make a copy of the list of pizzas, and call it friend_pizzas. Then, do the following:
+# 4-11. My Pizzas, Your Pizzas: Start with your program from Exercise 4-1 (page 56). Make a copy of the list of pizzas,
+# and call it friend_pizzas. Then, do the following:
 # Add a new pizza to the original list.
 # Add a different pizza to the list friend_pizzas.
-# Prove that you have two separate lists. Print the message My favorite pizzas are:, and then use a for loop to print the first list. Print the message My friend’s favorite pizzas are:, and then use a for loop to print the second list. Make sure each new pizza is stored in the appropriate list.
+# Prove that you have two separate lists. Print the message My favorite pizzas are:, and then use a for loop to print
+# the first list. Print the message My friend’s favorite pizzas are:, and then use a for loop to print the second list.
+# Make sure each new pizza is stored in the appropriate list.
+
+pizzas = ['White', 'Olive', 'Plain']
+
+friend_pizzas = pizzas[:]
+
+pizzas.append("Margherita")
+friends_pizzas.append("Pineapple")
+
+print("My favorite pizzas are:")
+for pizza in pizzas:
+    print(pizza)
+
+print("My friend's favorite pizzas are:")
+for pizza in friend_pizzas:
+    print(pizza)
