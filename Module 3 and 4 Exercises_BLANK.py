297adcb75dff33e01b044a90087428338d4da22d
Najiyah Williamson <najiyah.williamson@emory.edu>
2026-02-13 10:10:34 -0500

    fixed a typo

---
+++ Module 3 and 4 Exercises_BLANK.py
@@ -0,0 +1,49 @@
+
+#---------------------------------------------------
+# Application: Filtering patients with High Diabetes Risk
+#---------------------------------------------------
+patient_names = ["Alice", "Bob", "Charlie", "David", "Ted", "Sam", "Jennifer", "Tom"]
+glucose_levels = [140, 155, 130, 180, 200, 160, 120, 190]
+
+#---------------------Method 1---------------------
+#Create list of high risk patient names with their levels
+
+#Loop through glucose_levels and identify if a person's level is greater than 150
+
+    # If greater than 150, add the person's name and glucose level to "high risk patients" list
+
+
+#Sort the patients from highest to lowest risk and print
+
+#---------------------Method 2: List comprehensions---------------------
+
+
+#---------------------Method 3: enumerate() for loop ---------------------
+
+
+#List comprehension exercise - convert the enumerate for loop into a list comprehension
+
+
+
+#---------------------Method 4: zip() function ---------------------
+
+
+#List comprehension exercise - convert the zip() for loop into a list comprehension
+
+
+#-------------------- Method 5: enumerate()---------------------
+
+
+#------------------------------------------------------------------------------
+# Application with dictionaries
+#------------------------------------------------------------------------------
+patient_names = ["Alice", "Bob", "Charlie", "David", "Ted", "Sam", "Jennifer", "Tom"]
+glucose_levels = [140, 155, 130, 180, 200, 160, 120, 190]
+
+patients_dict = {"Alice":140, "Bob":155, "Charlie": 130, "David": 180,
+                 "Ted":200, "Sam": 160, "Jennifer": 120, "Tom":190}
+
+
+
+#-------------------- Dictionary Method 2 using zip()----------------------
+
