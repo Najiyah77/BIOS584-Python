897b03e32bcd198e34b8490ab8809776de2fe8d7
Najiyah Williamson <najiyah.williamson@emory.edu>
2026-02-13 11:46:01 -0500

    New assignments

--- Week 4 Module 3 and 4 Exercises.py
+++ Week 4 Module 3 and 4 Exercises.py
@@ -1,35 +1,55 @@
 
 #---------------------------------------------------
 # Application: Filtering patients with High Diabetes Risk
+# Name: Najiyah Williamson
+# Date: 02/13/26
 #---------------------------------------------------
 patient_names = ["Alice", "Bob", "Charlie", "David", "Ted", "Sam", "Jennifer", "Tom"]
 glucose_levels = [140, 155, 130, 180, 200, 160, 120, 190]
 
 #---------------------Method 1---------------------
 #Create list of high risk patient names with their levels
+high_risk_patients = []
 
 #Loop through glucose_levels and identify if a person's level is greater than 150
-
-    # If greater than 150, add the person's name and glucose level to "high risk patients" list
-
+for i in range(len(patient_names)):
+    if glucose_levels[i] >= 150: # If greater than 150, add the person's name and glucose level to "high risk patients" list
+        high_risk_patients.append((glucose_levels[i], patient_names[i]))
 
 #Sort the patients from highest to lowest risk and print
+high_risk_patients.sort(reverse=True)
+#THIS IS VALID: high_risk_patients = sorted(high_risk_patients, reverse=True)
+#WRONG: high_risk_patients = high_risk_patients.sort(reverse=True)
 
 #---------------------Method 2: List comprehensions---------------------
-
+high_risk_patients = sorted([[glucose_levels[i], patient_names[i]] for i in range(len(patient_names))
+                        if glucose_levels[i] > 150], reverse=True)
+print(high_risk_patients)
 
 #---------------------Method 3: enumerate() for loop ---------------------
-
+high_risk_patients = []
+for index, glucose in enumerate(glucose_levels):
+    if glucose > 150:
+        high_risk_patients.append((glucose, patient_names[index]))
 
 #List comprehension exercise - convert the enumerate for loop into a list comprehension
+high_risk_patients = [[glucose, patient_names[index]] for index, glucose in enumerate(glucose_levels) if glucose > 150]
 
-
+high_risk_patients.sort(reverse=True)
+print(high_risk_patients)
 
 #---------------------Method 4: zip() function ---------------------
+high_risk_patients = []
+for glucose, name in zip(glucose_levels, patient_names): #pairing up each item to create a sequence of each pairwise combination. glucose refers to first item (glucose_levels) and then name to patient_names)
+    if glucose > 150:
+        high_risk_patients.append((glucose, name))
 
+high_risk_patients.sort(reverse=True)
+print(high_risk_patients)
 
 #List comprehension exercise - convert the zip() for loop into a list comprehension
-
+high_risk_patients = sorted([[glucose, name] for glucose, name in zip(glucose_levels, patient_names) if glucose > 150], reverse=True)
+print(high_risk_patients)
 
 #-------------------- Method 5: enumerate()---------------------
 
